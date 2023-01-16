47ad0194e9b94225a5d6e1a77586bdf7d7ebf64c
aret <aret3737@gmail.com>
2023-01-16 05:20:42 +0200

    Did String exercises

--- Headers/StringExercisesLeetCode.hpp
+++ Headers/StringExercisesLeetCode.hpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <cctype>
+#include <unordered_map>
 
 using namespace std;
 
@@ -70,4 +72,146 @@ public:
     }
 };
 
+/* Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+Example 1:
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+Input: s = "loveleetcode"
+Output: 2 */
+class Solution3 {
+public:
+    int firstUniqChar(string myString) {
+        unordered_map<char, int> myMap;
+
+        for (const char& c : myString) {
+            myMap[c]++;
+        }
+        for (auto iter = myMap.begin(); iter != myMap.end(); ++iter) {
+        cout << iter->first << ": " << iter->second << " ";
+        }
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString[i];
+            if (myMap[c] == 1) {return i;}
+        }
+        return -1;
+    }
+};
+
+/* Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false */
+
+class Solution4 {
+public:
+    bool isAnagram(string OrigStr, string CompStr) {
+        unordered_map<char, int> myMap;
+
+        for (int i = 0; i < OrigStr.length(); i++) {
+            char c = OrigStr[i];
+            myMap[c]++;
+        }
+
+        for (auto iter = myMap.begin(); iter != myMap.end(); ++iter) { //showing on terminal
+        cout << iter->first << ": " << iter->second << " " << endl;
+        }
+        
+        for (int i = 0; i < CompStr.length(); i++) {
+            auto it = myMap.find(CompStr[i]);
+            if (it != myMap.end()) { 
+                it->second -= 1; //found letter, removing 1 from value
+                cout << "Removing letter " << CompStr[i] << endl;
+                cout << it->first << ": " << it->second << " " << endl;
+                if (it->second < 0) {return false;}
+            } else {cout << "Didnt found letter " << CompStr[i] << endl;
+                return false;}
+            
+        }
+
+        for (auto iter = myMap.begin(); iter != myMap.end(); ++iter) { //last check if any letter left
+            if(iter->second > 0) return false;
+        }
+        return true;
+    }
+};
+
+/* A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
+it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+ */
+class Solution5 {
+public:
+    bool isPalindrome(string myString) {
+        
+        myString.erase(remove_if(myString.begin(), myString.end(), [](char c){ return !isalnum(c); }), myString.end());
+        //removing non-alphanumeric chars with remove_if(isalnum(c))
+        transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
+        //transform big letter into small ones with ::tolower
+        if (myString.empty() || all_of(myString.begin(), myString.end(), ::isspace) || myString.length() == 1) {
+            return true;
+        }
+
+        cout << myString << " lenght is: " << myString.length()/2 << endl;
+        for (int i = 0, k = myString.length()-1; ((i <= myString.length()/2)&&(k >= myString.length()/2)); ) {
+            if (myString[i] != myString[k]) {
+                cout << "Letters " << myString[i] << " and " << myString[k] << " not equal at iteration k: " << k << " and i: " << i; 
+                return false;}
+            else {i++; k--; }
+            //cout << "Comparing i: " << myString[i] << " and k: " << myString[k] << endl;}
+        }
+        return true;
+    }
+};
+
+/* Write a function to find the longest common prefix string amongst an array of strings.
+If there is no common prefix, return an empty string "".
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings. */
+
+class Solution6 {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = "";
+        int smallestword = 200;
+        for (int k = 0; k < strs.size(); k++){ //counting size of words in vector
+                if (strs[k].length() < smallestword) {smallestword = strs[k].length();}
+        }
+
+        for (int i = 0; i < smallestword; i++){//iterating letters
+            char currentChar = strs[0][i]; //assigning first letter of first word
+            for (int k = 0; k < strs.size(); k++){ //iterating words in vector
+                if (currentChar==strs[k][i]){continue;} else {return prefix;}
+            }
+            //if just some word have common first letter, return {}
+            prefix+=currentChar;
+        }
+        return prefix;
+    }
+};
 #endif //STRINGEXERCISES_HPP
--- main.cpp
+++ main.cpp
@@ -22,18 +22,21 @@ main(){
     vector <int> v2 = {6, 89, 23, 56, 123, 59, 12, 87, 34, 65, 4567, 2, 1, 54, 15};
     vector <int> v3 = {1,4,-6,-9};
     vector <int> v4 = {0,0,3,3,4,2,4,0,8};
-    vector <char> s1 = {'H','a','n','n','a','h'};
-    vector <char> s2 = {'H','e','l','l','o'};
-    vector<char> s3 = {'A', ' ', 'm', 'a', 'n', ',', ' ', 'a', ' ', 'p', 'l', 'a', 'n', ',', ' ', 'a', ' ', 'c', 'a', 'n', 'a', 'l', ':', ' ', 'P', 'a', 'n', 'a', 'm', 'a'};
+    vector <char> c1 = {'H','a','n','n','a','h'};
+    vector <char> c2 = {'H','e','l','l','o'};
+    vector<char> c3 = {'A', ' ', 'm', 'a', 'n', ',', ' ', 'a', ' ', 'p', 'l', 'a', 'n', ',', ' ', 'a', ' ', 'c', 'a', 'n', 'a', 'l', ':', ' ', 'P', 'a', 'n', 'a', 'm', 'a'};
     int a1 = 123, a2 = 3456700, a3 = -592, a4 = 2;
-    Solution2 myObj;
-    myObj.reverse(a1);
+    string s1 = "raceacar", s2 = "hannah", s3 = "Do geese see God?", s4 = "abcdefghijklmnop", s5 = "reerac";
+    vector <string> vs1 = {"babe", "babushka", "babaika", "bababababab"};
+
+    Solution6 myObj;
+    cout << myObj.longestCommonPrefix(vs1);
     cout << endl;
-    myObj.reverse(a2);
+    /*cout << myObj.isPalindrome(s2);
     cout << endl;
-    myObj.reverse(a3);
+    cout << myObj.isPalindrome(s3);
     cout << endl;
-    myObj.reverse(a4);
+    myObj.reverse(a4); */
 
 
 }
